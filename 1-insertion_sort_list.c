c82df3ac4573a3d8db24d3e64530e507971f2eca
root <root@ip-10-42-8-59.ec2.internal>
2025-10-28 15:18:25 +0000

    task 0 task1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include"sort.h"
+/**
+* insertion_sort_list - sorts  a doubly linked list using Insertion sort algorithm
+* @list: list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp, *node;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	tmp = (*list)->next;
+	while (tmp)
+	{
+		node = tmp;
+		tmp = tmp->next;
+		while (node->prev && node->n < node->prev->n)
+		{
+			if (node->next)
+				node->next->prev = node->prev;
+			node->prev->next = node->next;
+			node->next = node->prev;
+			node->prev = node->next->prev;
+			if (node->prev)
+				node->prev->next = node;
+			node->next->prev = node;
+			if (!node->prev)
+				*list = node;
+			print_list(*list);
+		}
+	}
+}
